b6b450fc098861662961f67c304d7e8926136bcf
halenazsari <240541008@firat.edu.tr>
2025-10-23 21:30:26 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,10 @@
+public class ToplamaSeri {
+    public static void main(String[] args) {
+       // Sonucu Hesapla
+       int sonuc=1+2+3+4+5+6+7+8+9;
+
+       //Ekrana Yazdır
+         System.out.println("Toplam: " + sonuc);
+         
+    }
+}
