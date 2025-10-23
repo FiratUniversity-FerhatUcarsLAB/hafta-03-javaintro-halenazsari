5fc6108f458794f9285114c7e497f19187dbe4b7
halenazsari <240541008@firat.edu.tr>
2025-10-23 21:28:11 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,11 @@
+package gorev1-hello;
+
+public class DesenCizme {
+    public static void main ( String [] args ) {
+3 System . out . println ( " J A V V A " ) ;
+    System . out . println ( " J     A     V   V    A " ) ;
+    System . out . println ( " J    A A     V V    A A " ) ;
+    System . out . println ( " J   AAAAA     V    AAAAA " ) ;
+    System . out . println ( " J  A     A    V   A     A " ) ;
+    }
+}
