dafb2db1f90985f54e4166cfb54265d8c116ea74
halenazsari <240541008@firat.edu.tr>
2025-10-23 21:34:50 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,19 @@
+package JavaLab3.gorev4metinvestring;
+
+public class IsimKarti {
+    public static void main(String[] args) {
+        string isim = "Halenaz";
+        string soyisim = "SARI";
+        string ogrno = "240541008";
+        string bolum = "Yazılım Muhendisligi";
+
+        System.out.println("================================");
+        System.out.println("         OGRENNCİ BİLGİLERİ         ");
+        System.out.println("================================");
+    }   System.out.println(" Ad:" + isim);
+        System.out.println(" Soyad: " + soyisim);
+        System.out.println(" Ogrenci No: " + ogrno);
+        System.out.println(" Bolum: " + bolum);
+        System.out.println("================================");
+    }
+}
