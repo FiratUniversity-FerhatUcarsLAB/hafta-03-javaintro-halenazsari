1ca23f84be3088564ea0f1afe70025f75f062e30
halenazsari <240541008@firat.edu.tr>
2025-10-23 21:49:28 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,13 @@
+package JavaLab3.gorev5ciktiformatlari;
+
+public class UsluSayilarTablosu {
+    System.out.println("a a^2 a^3");
+    System.out.println( " - - - - - - - - - - - - - - - - - " ) ;
+    int a1 = 1;
+    System . out . println ( a1 + " "+ ( a1 * a1 ) + " " + ( a1 * a1 * a1 ) ) ;
+    int a2 = 2;
+    System . out . println ( a2 + " "+ ( a2 * a2 ) + " " + ( a2 * a2 * a2 ) ) ;
+    int a3 = 3;
+    System . out . println ( a3 + " "+ ( a3 * a3 ) + " " + ( a3 * a3 * a3 ) ) ;
+
+}
